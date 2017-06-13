4e8d4c07d5d06e3b5352f438d200bd0a3f00e4f1
Adam Cowley <adam@wecommit.co>
2017-06-13 10:19:51 +0100

    Moved src/co to src/java/co - gradle will not build it otherwise.  Set neo4j dependency to compileOnly

--- src/main/java/co/wecommit/neoprocedure/NeoProcedure.java
+++ src/main/java/co/wecommit/neoprocedure/NeoProcedure.java
@@ -1,17 +1,25 @@
 package co.wecommit.neoprocedure;
 
-import java.util.stream.*;
 import org.neo4j.graphdb.*;
-import org.neo4j.procedure.*;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Mode;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+import org.neo4j.procedure.UserFunction;
 
-import org.neo4j.procedure.Procedure.Mode;
+import java.util.stream.Stream;
+
+
+
+
 
 public class NeoProcedure {
 
     @Context
-    private GraphDatabaseService db;
+    public GraphDatabaseService db;
 
-    class SomethingResult {
+    public static class SomethingResult {
         public String output;
 
         public SomethingResult(String output) {
@@ -22,12 +30,13 @@ public class NeoProcedure {
     @Procedure(value = "wecommit.dosomething", mode= Mode.WRITE)
     @Description("Do something")
     public Stream<SomethingResult> dosomething(@Name("input") String input) {
-        return new Stream.of(SomethingResult(input));
+        return Stream.of(new SomethingResult(input));
     }
 
     @UserFunction
+
     @Description("co.wecommit.neoprocedure.somethingelse - do something else")
-    public String somethingelse(String input) {
+    public String somethingelse(@Name("input") String input) {
         return input;
     }
 }
